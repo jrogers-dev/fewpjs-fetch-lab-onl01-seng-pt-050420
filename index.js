458e88c9ac33768a638e309d47f4bd745e1b5670
James Rogers <jrogers.dev@gmail.com>
2020-11-18 01:23:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function fetchBooks() {
-  let superfetch = fetch('https://anapioficeandfire.com/api/books');
+  let superfetch = fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json)
+  });
+  
+  return superfetch;
 }
 
 function renderBooks(books) {
