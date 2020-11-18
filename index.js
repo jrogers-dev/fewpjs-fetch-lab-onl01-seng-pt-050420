feed012f9c3573104e8a8413e1f6858c602381c6
James Rogers <jrogers.dev@gmail.com>
2020-11-18 01:33:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
     .then(function(response) { return response.json(); })
-    .then(function(response) { console.log(response); })
+    .then(function(response) { return response; })
   ;
   
   
